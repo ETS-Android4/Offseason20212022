fe144358be601d885c2bd6f0b3905b5af07f5cad
ktphan0 <ktphan0@gmail.com>
2022-03-15 16:43:25 -0700

    a test mecanum drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecExperimental.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class MecExperimental extends LinearOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        waitForStart();
+
+        //if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double r = gamepad1.right_stick_x;
+
+            motorFrontLeft.setPower(y + x + r);
+            motorBackLeft.setPower(y - x + r);
+            motorFrontRight.setPower(y - x - r);
+            motorBackRight.setPower(y + x - r);
+
+        }
+    }
+}
