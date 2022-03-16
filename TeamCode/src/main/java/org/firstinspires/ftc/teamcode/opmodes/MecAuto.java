8dce5e7f393dfaee0dc40c713d1ac9ab8eae2b6b
ktphan0 <ktphan0@gmail.com>
2022-03-15 17:52:31 -0700

    added empty auto class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecAuto.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous
+public class MecAuto extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecExperimental.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecExperimental.java
@@ -19,9 +19,9 @@ public class MecExperimental extends LinearOpMode{
         //if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double y = gamepad1.right_stick_x; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double r = gamepad1.right_stick_x;
+            double r = gamepad1.left_stick_y;
 
             motorFrontLeft.setPower(y + x + r);
             motorBackLeft.setPower(y - x + r);
