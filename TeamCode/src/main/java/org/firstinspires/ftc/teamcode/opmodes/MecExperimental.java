636c7c9109ec2a47c227f87c2171fdc55eb4dc3b
ktphan0 <ktphan0@gmail.com>
2022-04-01 17:42:30 -0700

    add gryo to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecExperimental.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecExperimental.java
@@ -1,14 +1,31 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 @TeleOp
 public class MecExperimental extends LinearOpMode{
+
+    BNO055IMU imu;
+    Orientation angles;
+
     @Override
     public void runOpMode() throws InterruptedException {
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
         DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
@@ -24,6 +41,14 @@ public class MecExperimental extends LinearOpMode{
         //if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+            //gyro
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addData("Heading", angles.firstAngle);
+            telemetry.addData("Roll", angles.secondAngle);
+            telemetry.addData("Pitch", angles.thirdAngle);
+            telemetry.update();
+
+            //drive controls
             double r = -gamepad1.right_stick_x; // Remember, this is reversed!
             double x = gamepad1.dpad_left ? -1 : gamepad1.dpad_right ? 1 : 0; // Counteract imperfect strafing
             double y = gamepad1.dpad_down ? 1 : gamepad1.dpad_up ? -1 : 0; // google if statement ternery operator if ? and : confusing
